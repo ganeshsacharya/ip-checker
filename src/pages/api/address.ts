dced7276b359501bfacfb0cf48df2a9f5b38f5b7
Ganesh S Acharya <ganesh.a@juegostudio.com>
2023-03-25 18:34:27 +0530

    bugfix-6

--- src/pages/api/address.ts
+++ src/pages/api/address.ts
@@ -1,19 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {  NextApiRequest, NextApiResponse } from 'next'
 
-import type { NextRequest } from 'next/server'
-
-
-type Data = {
-  ip: string
+type IResponseType = {
+  client_ip_addresss: string
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<any>
+  res: NextApiResponse<IResponseType>
 ) {
   const forwarded = req.headers['x-forwarded-for'];
   const ip = typeof forwarded === 'string' ? forwarded.split(/, /)[0] : req.socket.remoteAddress;
-  console.log(ip);
-  res.status(200).json({ip})
+  res.status(200).json({
+    client_ip_addresss: ip as string
+  })
 }
