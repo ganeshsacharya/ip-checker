c4fa1fe08cff0124bc0807d29e4617f5cff14de7
Ganesh S Acharya <ganesh.a@juegostudio.com>
2023-03-25 08:28:35 +0530

    bugfix-2

--- src/pages/api/address.ts
+++ src/pages/api/address.ts
@@ -1,14 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type {  NextApiRequest, NextApiResponse } from 'next'
+
+import type { NextRequest } from 'next/server'
+
 
 type Data = {
   ip: string
 }
 
 export default function handler(
-  req: NextApiRequest,
+  req: NextRequest,
   res: NextApiResponse<any>
 ) {
-  console.log(req.connection.remoteAddress)
-  res.status(200).json({ ip: req.connection.remoteAddress })
+  res.status(200).json({ ip: req.ip})
 }
